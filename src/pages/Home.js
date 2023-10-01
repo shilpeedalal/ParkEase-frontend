cd629722e910f0a707b0cf6268847128f748bb83
Shilpee <shilpeedalal937@gmail.com>
2023-10-01 14:28:37 +0530

    initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './../css/home.scss'
+
+function Home() {
+  return (
+    <div>
+      <div className='banner'>
+        <div className='overlay'>
+          <h1>Book your parking slot...</h1>
+        </div>
+      </div> 
+    </div>
+  )
+}
+
+export default Home
